189a9745072a57fc45231bff4d5b3474a0133a42
Chandra kishor Gauro <ckgauro@gmail.com>
2020-10-03 21:34:55 -0400

    Beer Post error fix

--- mssc-brewery/src/main/java/com/gauro/msscbrewery/web/controller/BeerController.java
+++ mssc-brewery/src/main/java/com/gauro/msscbrewery/web/controller/BeerController.java
@@ -2,6 +2,7 @@ package com.gauro.msscbrewery.web.controller;
 
 import com.gauro.msscbrewery.services.BeerService;
 import com.gauro.msscbrewery.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import java.util.UUID;
  * Created by chandra
  */
 
+@Slf4j
 @RequestMapping("/api/v1/beer")
 @RestController
 public class BeerController {
@@ -27,12 +29,13 @@ public class BeerController {
     public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId")UUID beerId){
         return new ResponseEntity<>(beerService.getBeerByID(beerId), HttpStatus.OK);
     }
-    @PostMapping
-    public ResponseEntity<BeerDto> handlePost(BeerDto beerDto){
+    @PostMapping(consumes = "application/json")
+    public ResponseEntity<BeerDto> handlePost(@RequestBody BeerDto beerDto){
+        log.info("BeerDto is called -->"+beerDto);
         BeerDto saveDto=beerService.saveNewBeer(beerDto);
-
+        log.info("BeerDto is called -->"+saveDto);
         HttpHeaders headers=new HttpHeaders();
-        headers.add("Location","/api/v1/beer"+saveDto.getId().toString());
+        headers.add("Location","/api/v1/beer/"+saveDto.getId().toString());
         return new ResponseEntity<>(headers,HttpStatus.CREATED);
     }
 }
