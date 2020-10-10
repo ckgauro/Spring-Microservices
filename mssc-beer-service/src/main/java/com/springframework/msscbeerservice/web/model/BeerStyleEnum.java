70a0efcc9930b680f3db6dadafaaacf99b2ae2c3
Chandra kishor Gauro <ckgauro@gmail.com>
2020-10-10 14:46:53 -0400

    Spring MicroServices mssc-beer-service second

---
+++ mssc-beer-service/src/main/java/com/springframework/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,8 @@
+package com.springframework.msscbeerservice.web.model;
+
+/**
+ * @author Chandra
+ */
+public enum BeerStyleEnum {
+    LARGER, PILSNER,STOUT,GOSE,PORTER,ALE,WHEAT,IPA,PALE_ALE,SAISON
+}
