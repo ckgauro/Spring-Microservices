1b989b2af1387f87a5d49bf78794a26d9fb1cf85
Chandra kishor Gauro <ckgauro@gmail.com>
2020-10-12 11:54:59 -0400

    Step Services

--- in28Minutes/springExampleinIn28minutes/src/main/java/com/gauro/springExampleinIn28minutes/controller/LoginController.java
+++ in28Minutes/springExampleinIn28minutes/src/main/java/com/gauro/springExampleinIn28minutes/controller/LoginController.java
@@ -1,24 +1,54 @@
 package com.gauro.springExampleinIn28minutes.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import lombok.extern.slf4j.Slf4j;
+import com.gauro.springExampleinIn28minutes.services.LoginService;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.log4j.Logger;
 @Slf4j
 @Controller
 public class LoginController {
+	final static Logger logger = Logger.getLogger(LoginController.class);
 	
+	@Autowired
+	LoginService loginService;
 //	@RequestMapping("/login")
 //	@ResponseBody
 //	public String loginMessage() {
 //		return "login";
 //	}
 
-	@RequestMapping("/login")
-	public String loginMessage(){
+	@RequestMapping(value="/login", method = RequestMethod.GET)
+	public String showLoginPage(){		
 		
 		return "login";
+	}
+	
+	@RequestMapping(value="/login", method = RequestMethod.POST)
+	public String handleLogin(ModelMap model, @RequestParam String name, @RequestParam String password) {
+		logger.info("handleLogin=====>");
+		if(! loginService.validateUser(name,password)) {
+			return "login";
+		}
+		
+		model.put("name", name);
+		return "welcome";
+		
+	}
+	
+
+	@RequestMapping("/")
+	public String handler (Model model) {
+		model.addAttribute("msg",
+				"a jar packaging example");
+		return "myView";
 	}
 }
--- in28Minutes/springExampleinIn28minutes/src/main/webapp/WEB-INF/jsp/login.jsp
+++ in28Minutes/springExampleinIn28minutes/src/main/webapp/WEB-INF/jsp/login.jsp
@@ -5,7 +5,11 @@
 </head>
 
 <body>
-My First JSP!!
+<form method="post">
+Name: <input type="text" name="name">
+Password: <input type="password" name="password">
+<input type="submit"> 
+</form>
 </body>
 
 </html>
