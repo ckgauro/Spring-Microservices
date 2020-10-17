bf2e846a6c613d1cb124f351590cafdfe771128d
Chandra kishor Gauro <ckgauro@gmail.com>
2020-10-17 10:11:32 -0400

    SpringExamples Chp 25

--- in28Minutes/springExampleinIn28minutes/src/main/java/com/gauro/springExampleinIn28minutes/SpringExampleinIn28minutesApplication.java
+++ in28Minutes/springExampleinIn28minutes/src/main/java/com/gauro/springExampleinIn28minutes/SpringExampleinIn28minutesApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 @ComponentScan("com.gauro.springExampleinIn28minutes.web")
 public class SpringExampleinIn28minutesApplication {
 
 	public static void main(String[] args) {
+		log.info("Logg");
 		SpringApplication.run(SpringExampleinIn28minutesApplication.class, args);
 	}
 
