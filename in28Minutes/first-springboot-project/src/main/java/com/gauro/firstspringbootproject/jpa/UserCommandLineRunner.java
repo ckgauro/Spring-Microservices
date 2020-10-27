1e6375b9b430f5bd257677195a8db3891dfefd52
Chandra kishor Gauro <ckgauro@gmail.com>
2020-10-26 20:32:49 -0400

    Lect 78

--- in28Minutes/first-springboot-project/src/main/java/com/gauro/firstspringbootproject/jpa/UserCommandLineRunner.java
+++ in28Minutes/first-springboot-project/src/main/java/com/gauro/firstspringbootproject/jpa/UserCommandLineRunner.java
@@ -1,6 +1,8 @@
 package com.gauro.firstspringbootproject.jpa;
 
 import java.util.List;
+import java.util.Spliterator;
+import java.util.function.Predicate;
 import java.util.stream.StreamSupport;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,22 +13,37 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
-public class UserCommandLineRunner implements CommandLineRunner{
+public class UserCommandLineRunner implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository repository;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
-		
-		repository.save(new User("chandra", "Admin"));
+		Predicate<User> pUserAdmin = el -> el.getRole().equals("Admin");
+		Predicate<User> pUserName = el -> el.getName().startsWith("R");
+
+		repository.save(new User("Chandra", "Admin"));
 		repository.save(new User("Ravi", "User"));
 		repository.save(new User("Raju", "Admin"));
 		repository.save(new User("Satih", "User"));
-		
-		Iterable<User> lsUser=repository.findAll();
-		StreamSupport.stream( lsUser.spliterator(), false).forEach(System.out::println);
-		
+
+		Iterable<User> lsUser = repository.findAll();
+		StreamSupport.stream(lsUser.spliterator(), false).forEach(System.out::println);
+
+		StreamSupport.stream(repository.findByRole("Admin").spliterator(), false).filter(pUserAdmin)
+				.forEach(el -> log.info(el.toString()));
+
+		log.info("----Find By Role----");
+		StreamSupport.stream(repository.findByRole("Admin").spliterator(), false)
+				.forEach(el -> log.info(el.toString()));
+		log.info("----Find By Name----");
+		StreamSupport.stream(repository.findByName("Chandra").spliterator(), false)
+				.forEach(el -> log.info(el.toString()));
+		log.info("----Find by Name like");
+		StreamSupport.stream(repository.findAll().spliterator(), false).filter(pUserName)
+				.forEach(el -> log.info(el.toString()));
+
 	}
 
 }
--- in28Minutes/first-springboot-project/src/main/java/com/gauro/firstspringbootproject/jpa/UserRepository.java
+++ in28Minutes/first-springboot-project/src/main/java/com/gauro/firstspringbootproject/jpa/UserRepository.java
@@ -1,9 +1,11 @@
 package com.gauro.firstspringbootproject.jpa;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-
-	
+	List<User> findByRole(String role);
+	List<User> findByName(String name);
 
 }
