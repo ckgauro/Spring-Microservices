b0eecb1177b1871bb0fbd0a26e0e280d7d2c2f25
Chandra kishor Gauro <ckgauro@gmail.com>
2020-10-31 07:32:36 -0400

    Syrvey

---
+++ in28Minutes/first-springboot-project/src/test/java/com/gauro/firstspringbootproject/controller/SurveyControllerIT.java
@@ -0,0 +1,17 @@
+package com.gauro.firstspringbootproject.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+class SurveyControllerIT {
+
+	@Test
+	void test() {
+		
+	}
+
+}
